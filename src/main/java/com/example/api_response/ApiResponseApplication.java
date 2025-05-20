4d8a40354f06816a7354e43c1de6d3477fa76101
radiant1010 <doomsday123@naver.com>
2025-05-20 19:52:12 +0900

    공통 응답 모듈 초기 설정

---
+++ src/main/java/com/example/api_response/ApiResponseApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_response;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResponseApplication.class, args);
+	}
+
+}
